e3c45fc943ee7af8a057fc251a114ac259cf74cc
Lucas <Rodriguesdesouzal28@gmail.com>
2023-11-29 20:20:42 -0300

    feat: init title

--- src/app/[tenant]/page.tsx
+++ src/app/[tenant]/page.tsx
@@ -1,7 +1,21 @@
-
+import "../globals.css";
 
 export default function Home() {
   return (
-   <div>hello</div>
-  )
+    <div className="bg-#fff">
+      <header className="bg-#f9f9fb pt-11 pr-6 pl-6 pb-7">
+        <div className="">
+          <div>
+            <div className="font-medium text-2xl text-#1b1b1b">
+              Seja Bem-Vindo (a) 👋
+            </div>
+            <div className="font-normal text-base text-gray-600 opacity-80">O que deseja para hoje?</div>
+          </div>
+          <div></div>
+        </div>
+
+        <div className="">Busca</div>
+      </header>
+    </div>
+  );
 }
