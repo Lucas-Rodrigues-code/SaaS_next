c6d2b7da57de6585bce500e041e841197445762f
Lucas <Rodriguesdesouzal28@gmail.com>
2024-01-16 19:45:45 -0300

    feat: layout login

--- src/app/[tenant]/login/page.tsx
+++ src/app/[tenant]/login/page.tsx
@@ -11,6 +11,7 @@ import Head from "next/head";
 import Header from "@/app/components/Header";
 import InputField from "@/app/components/InputField";
 import { Button } from "@/app/components/Button";
+import Link from "next/link";
 
 type Props = {
   params: {
@@ -21,6 +22,7 @@ export default function Login({ params }: Props) {
   const router = useRouter();
 
   const api = useApi();
+  
 
   const tenant = /* await  */ api.getTenant(params.tenant);
 
@@ -44,11 +46,15 @@ export default function Login({ params }: Props) {
 
   function handleSubmit() {}
 
+  function handleSignUp() {
+    router.push(`/${tenantContext?.slug}/signUp`)
+  }
+
   if (!tenantContext) {
     return <div>...</div>;
   }
   return (
-    <div className="bg-#fff pt-12" style={{padding:"50px 24px"}}>
+    <div className="bg-#fff pt-12" style={{ padding: "50px 24px" }}>
       <Head>
         <title>Login | {tenantContext?.name}</title>
       </Head>
@@ -56,31 +62,78 @@ export default function Login({ params }: Props) {
         color={tenantContext?.mainColor}
         backHref={`/${tenantContext?.slug}`}
       />
-      <InputField
-        color={tenantContext.mainColor}
-        placeHolder="Digite seu e-mail"
-        value={email}
-        onChange={setEmail}
-      />
-      <InputField
-        color={tenantContext.mainColor}
-        placeHolder="Digite sua senha"
-        value={password}
-        onChange={setPassword}
-        password
-      />
-      <Button
-        color={tenantContext.mainColor}
-        label="Entrar"
-        onClick={handleSubmit}
-        fill
-      />
-       <Button
-        color={tenantContext.mainColor}
-        label="Entrar"
-        onClick={handleSubmit}
-       
-      />
+      <div className="font-bold text-3xl mt-5 mb-10 text-center">
+        {tenantContext.name}
+      </div>
+      <div
+        className="font-normal text-lg leading-5 text-center pb-10"
+        style={{
+          color: "rgba(27,27,27,0.8)",
+          margin: "auto",
+          width: "223px",
+          borderBottom: "1.5px solid #000",
+          position: "relative",
+          borderBottomColor:tenantContext.mainColor
+        }}
+      >
+        Use suas credenciais para realizar o login.
+      </div>
+      <div
+        style={{ borderTop: "1.5px solid #e2e2e2", marginTop: "-1.5px " }}
+      ></div>
+      <div className="mt-14">
+        <div className="mt-8">
+          <InputField
+            color={tenantContext.mainColor}
+            placeHolder="Digite seu e-mail"
+            value={email}
+            onChange={setEmail}
+          />
+        </div>
+        <div className="mt-8">
+          <InputField
+            color={tenantContext.mainColor}
+            placeHolder="Digite sua senha"
+            value={password}
+            onChange={setPassword}
+            password
+          />
+        </div>
+        <div className="mt-8 mb-8">
+          <Button
+            color={tenantContext.mainColor}
+            label="Entrar"
+            onClick={handleSubmit}
+            fill
+          />
+        </div>
+      </div>
+      <div
+        className="mt-8 font-normal text-base text-center pb-16"
+        style={{
+          margin: "auto",
+          color: "rgba(27,27,27,0.8)",
+          width: "fit-content",
+          position: "relative",
+          borderBottom: "1.5px solid #000",
+          borderBottomColor:tenantContext.mainColor
+        }}
+      >
+        Esqueceu sua senha ?{" "}
+        <a className="font-semibold no-underline" style={{ color: tenantContext.mainColor }}>
+          <Link href={`/${tenantContext.slug}/forget`}>Clique aqui</Link>
+        </a>
+      </div>
+      <div
+        style={{ borderTop: "1.5px solid #e2e2e2", marginTop: "-1.5px " }}
+      ></div>
+      <div className="mt-8">
+        <Button
+          color={tenantContext.mainColor}
+          label="Quero me cadastrar"
+          onClick={handleSignUp}
+        />
+      </div>
     </div>
   );
 }
